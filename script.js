e9d0ce210f902a443709e3230e004253b91905ed
PGX <snakekillah217@gmail.com>
2025-02-23 18:05:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+	async function acortarUrl(link) {
+    const response = await fetch('https://acut0x.onrender.com/', {
+        method: 'POST',
+        body: new URLSearchParams({ url: link }),
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    });
+    const html = await response.text();
+    return new DOMParser().parseFromString(html, 'text/html').querySelector('a.acortada').href;
+}
 
-document.getElementById('btn').addEventListener('click', (event) => {
+
 const text = document.getElementById('text').value;
 const name = document.getElementById('filename').value;
 const blob = new Blob([text], { type: "text/plain" });
@@ -13,7 +22,18 @@ fetch("https://tmpfiles.org/api/v1/upload", {
 .then((data) => {
  if (data.status === "success") {
   const downloadLink = data.data.url;
-  document.getElementById('link').innerHTML = `<a href="${downloadLink.replace('.org/', '.org/dl/')}">LINK GENERADO</a>`;
+  const urlTMP=`${downloadLink.replace('.org/', '.org/dl/')}`;
+  (async () => {
+    try {
+        const shortUrl = await acortarUrl(urlTMP);
+        document.getElementById('link').innerHTML = `<a href="${shortUrl}">LINK GENERADO</a>`;
+    } catch (error) {
+        console.error('Error al acortar la URL:', error.message);
+    }
+})();
+
+
+
  } else {
   console.error("Error al subir el archivo:", data);
  }
@@ -21,4 +41,4 @@ fetch("https://tmpfiles.org/api/v1/upload", {
 .catch((error) => {
  console.error("Error en la solicitud:", error);
 });
-});
+
